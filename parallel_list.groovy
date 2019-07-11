c758a9bd4851b43809a75c39255e77315efe748f
Hung Nguyen <nguyendanghung2008@gmail.com>
2019-07-11 15:45:48 +0100

    Create parallel_list.groovy

---
+++ parallel_list.groovy
@@ -0,0 +1,37 @@
+var listStags = ["stage test one", "stage test twoo"]
+
+pipeline {
+    agent none
+    stages {
+        stage('Run Tests') {
+            parallel {
+                stage('Test On Windows') {
+                    
+                    steps {
+                       println('Test on Windows')
+                    }
+                    
+                }
+                stage('Test On Linux') {
+                    stage('Test on Linux 1') {
+                        steps {
+                           println('Test on Linux 1)
+                        }
+                    }
+                 stage('Test on linux 2') {
+                     steps{
+                        println('Test on Linux 2')
+                     }
+                 }
+                    
+                }
+                
+            }
+        }
+        stage('Deployment') {
+            steps {
+                println('Deployment')
+            }
+        }
+    }
+}
